038ef260d5a20e6c8b23f8cee1ef3c91069c2e05
Mike Wallio <michael.wallio@outlook.com>
2022-05-22 21:38:40 -0400

    Add cars to `-l` flag

--- sl.h
+++ sl.h
@@ -108,7 +108,7 @@
 #define LCAR1  L"____________________ "
 #define LCAR2  L"|  ___ ___ ___ ___ | "
 #define LCAR3  L"|  |_| |_| |_| |_| | "
-#define LCAR4  L"|__________________| "
+#define LCAR4  L"|  %-15.15s |  "
 #define LCAR5  L"|__________________| "
 #define LCAR6  L"   (O)        (O)    "
 
