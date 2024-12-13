4cea1ce66951bbac4ff438826812ab2f1ca00384
Mike Wallio <michael.wallio@outlook.com>
2024-12-13 02:23:36 +0000

    Implement `my_mvaddstr` using crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use core::time;
 use std::fs;
-use std::io::{stdin, stdout, BufRead, IsTerminal, Stdin, Stdout};
+use std::io::{stdin, stdout, BufRead, IsTerminal, Stdin, Stdout, Write};
 use std::thread::sleep;
 
 use clap::{command, Parser};
 
+use crossterm::terminal::{Clear, ClearType};
+use crossterm::{cursor, style::Print, terminal, ExecutableCommand, QueueableCommand};
 use freopen::{reopen_stdin, reopen_stdout};
 use libc::{c_int, wchar_t};
 use sl::{print_c51, print_d51, print_sl, set_locale};
@@ -39,12 +41,54 @@ pub static mut COLS: i32 = 0;
 pub static mut LINES: i32 = 0;
 
 #[no_mangle]
-pub extern "C" fn my_mvaddstr(_y: c_int, _x: c_int, _str: *const wchar_t) -> i32 {
-    return 1;
-    // let mut x = x;
-    // if let Ok(characters) = unsafe { CStr::from_ptr(str).to_str() } {
-    //     characters
-    // }
+pub extern "C" fn my_mvaddstr(y: c_int, x: c_int, str: *const wchar_t) -> i32 {
+    if y < 0 || y > unsafe { LINES } - 1 {
+        return -11;
+    }
+    let mut x = x;
+
+    #[cfg(target_family = "windows")]
+    type CCStr = widestring::U16CStr;
+    #[cfg(target_family = "unix")]
+    type CCStr = widestring::U32CStr;
+
+    let characters = unsafe { CCStr::from_ptr_str(str) };
+    if (x + characters.len() as i32) < 0 {
+        return 1;
+    }
+
+    let mut stdout = stdout();
+    if let Ok(mut queue) = stdout.queue(cursor::MoveTo(0, 0)) {
+        for c in characters.chars() {
+            x += 1;
+            if x < 0 {
+                continue;
+            }
+
+            if x > unsafe { COLS } - 1 {
+                break;
+            }
+
+            match queue.queue(cursor::MoveTo(x as u16, y as u16)) {
+                Ok(q) => queue = q,
+                Err(_) => return 1,
+            }
+
+            let c = if let Ok(c) = c { c } else { ' ' };
+
+            match queue.queue(Print(c)) {
+                Ok(q) => queue = q,
+                Err(_) => return 1,
+            }
+        }
+
+        match stdout.flush() {
+            Ok(_) => 0,
+            Err(_) => 1,
+        }
+    } else {
+        return 1;
+    }
 }
 
 fn main() {
@@ -66,14 +110,17 @@ fn main() {
         files
     };
 
-    let stdout = stdout();
+    let mut stdout = stdout();
     if !Stdout::is_terminal(&stdout) {
         names.iter().for_each(|n| println!("{}", n));
         reopen_stdout().unwrap();
     }
 
+    stdout.execute(cursor::Hide).unwrap();
     unsafe {
         set_locale();
+        update_size();
+
         let mut x = COLS - 1;
         let print_train = if args.logo {
             print_sl
@@ -91,9 +138,22 @@ fn main() {
             sl::FLY = 1;
         }
 
+        stdout.queue(Clear(ClearType::All)).unwrap();
         while print_train(x, names.iter().map(String::as_ref)) == 0 {
             x -= 1;
             sleep(time::Duration::from_micros(40000));
+            update_size();
         }
+    }
+}
+
+fn update_size() {
+    if let Ok((cols, lines)) = terminal::size() {
+        unsafe {
+            COLS = cols as i32;
+            LINES = lines as i32;
+        }
+    } else {
+        eprintln!("Failed to get terminal size");
     }
 }
