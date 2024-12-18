1977d996803a7944a10054b92908359d94045b96
Mike Wallio <michael.wallio@outlook.com>
2024-12-18 01:12:29 +0000

    Add rust-analyzer

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo dnf install -y cmake ninja-build dnf-plugins-core cargo @c-development @rpm-development-tools rpkg rustfmt rust-src
+sudo dnf install -y cmake ninja-build dnf-plugins-core cargo @c-development @rpm-development-tools rpkg rustfmt rust-src rust-analyzer
