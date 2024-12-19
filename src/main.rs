3a542764c041e7d3dc3b20c694d247f84996491a
Mike Wallio <michael.wallio@outlook.com>
2024-12-19 10:57:46 -0500

    Feat: Async reading from stdin (#44)
    * Enable async reading from stdin pipe
    * Mild code cleanup
    * Bump version number
    * Update README

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use clap::Parser;
 use cli::CliOptions;
 use core::time;
-use crossterm::event::{poll, read, Event, KeyCode, KeyEvent, KeyModifiers};
+use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyModifiers};
 use crossterm::terminal::{Clear, ClearType};
 use crossterm::{cursor, terminal, ExecutableCommand, QueueableCommand};
-use sl::{print_c51, print_d51, print_sl, set_locale, COLS, LINES};
+use filedescriptor::{Error, FileDescriptor};
+use sl::{print_c51, print_d51, print_sl, update_locale, COLS, LINES};
 use std::fs;
-use std::io::{stdin, stdout, BufRead, Error, IsTerminal, Stdin, Write};
+use std::io::{stdin, stdout, BufRead, BufReader, IsTerminal, Stdin, Write};
+use std::sync::mpsc::Receiver;
 
 mod cli;
 mod sl;
@@ -14,78 +16,105 @@ mod sl;
 fn main() -> Result<(), Error> {
     let args = CliOptions::parse();
     let stdin = stdin();
-    let names: Vec<String> = if !Stdin::is_terminal(&stdin) {
-        let names: Vec<String> = stdin
-            .lock()
-            .lines()
-            .filter_map(|l| match l {
-                Ok(s) => Some(s),
-                Err(_) => None,
-            })
-            .collect();
-        names
-    } else if args.files {
-        vec![]
-    } else {
-        let mut files: Vec<String> = fs::read_dir(".")?
-            .filter_map(|p| match p {
-                Ok(p) => Some(String::from(p.file_name().to_str()?)),
-                Err(_) => None,
-            })
-            .filter(|s| args.accident || !s.starts_with('.'))
-            .collect();
-        files.sort();
-        files
-    };
+    let names_receiver = cars_receiver(&args, stdin)?;
+
+    terminal::enable_raw_mode()?;
 
     let mut stdout = stdout();
-    terminal::enable_raw_mode()?;
     stdout.execute(cursor::Hide)?;
-    unsafe {
-        set_locale();
-        update_size()?;
 
-        let mut x = COLS - 1;
-        let print_train = if args.logo {
-            print_sl
-        } else if args.c51 {
-            print_c51
-        } else {
-            print_d51
-        };
+    update_locale();
+    update_size()?;
 
-        if args.accident {
+    let print_train = if args.logo {
+        print_sl
+    } else if args.c51 {
+        print_c51
+    } else {
+        print_d51
+    };
+
+    if args.accident {
+        unsafe {
             sl::ACCIDENT = 1;
         }
+    }
 
-        if args.fly {
+    if args.fly {
+        unsafe {
             sl::FLY = 1;
         }
+    }
 
-        stdout.queue(Clear(ClearType::All))?;
-        while print_train(x, names.iter().map(String::as_ref)) == 0 {
-            stdout.flush()?;
-            x -= 1;
-            if poll(time::Duration::from_micros(40_000))? {
-                if let Event::Key(KeyEvent {
+    let mut x = unsafe { COLS - 1 };
+    stdout.queue(Clear(ClearType::All))?;
+    let mut names: Vec<String> = vec![];
+    loop {
+        match names_receiver.try_recv() {
+            Ok(name) => {
+                names.push(name);
+            }
+            Err(_) => {}
+        }
+
+        if print_train(x, &names.iter().map(String::as_ref).collect::<Vec<&str>>()) != 0 {
+            break;
+        }
+
+        stdout.flush()?;
+        x -= 1;
+
+        if crossterm::event::poll(time::Duration::from_micros(40_000))? {
+            match read()? {
+                Event::Key(KeyEvent {
                     code: KeyCode::Char('c'),
                     modifiers: KeyModifiers::CONTROL,
                     ..
-                }) = read()?
-                {
-                    break;
-                }
+                }) => break,
+                Event::Resize(cols, lines) => unsafe {
+                    stdout.queue(Clear(ClearType::All))?;
+                    COLS = cols as i32;
+                    LINES = lines as i32;
+                },
+                _ => {}
             }
-
-            // "handle" resize
-            update_size()?;
         }
     }
+
     stdout.queue(Clear(ClearType::All))?.queue(cursor::Show)?;
     stdout.flush()?;
     terminal::disable_raw_mode()?;
 
     Ok(())
+}
+
+fn cars_receiver(args: &CliOptions, stdin: Stdin) -> Result<Receiver<String>, Error> {
+    let (sender, receiver) = std::sync::mpsc::channel();
+    if !Stdin::is_terminal(&stdin) {
+        let stdin_file = FileDescriptor::dup(&stdin.lock())?;
+        std::thread::spawn(move || {
+            let reader = BufReader::new(stdin_file);
+            reader.lines().for_each(|line| match line {
+                Ok(line) => sender.send(line).unwrap(),
+                Err(_) => {}
+            });
+        });
+    } else if args.files {
+        // Nothing to send
+    } else {
+        let accident = args.accident;
+        std::thread::spawn(move || {
+            fs::read_dir(".").unwrap().for_each(|p| {
+                let p = p.unwrap();
+                let name = p.file_name().to_str().unwrap().to_string();
+                if accident || !name.starts_with('.') {
+                    sender.send(name).unwrap();
+                }
+            });
+        });
+    };
+
+    Ok(receiver)
 }
 
 fn update_size() -> Result<(), Error> {
