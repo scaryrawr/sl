4e2ff70d4921c27965769b7d26a15a543fd55581
Mike Wallio <michael.wallio@outlook.com>
2024-12-18 21:14:47 -0500

    Mild code cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyModifiers};
 use crossterm::terminal::{Clear, ClearType};
 use crossterm::{cursor, terminal, ExecutableCommand, QueueableCommand};
 use filedescriptor::{Error, FileDescriptor};
-use sl::{print_c51, print_d51, print_sl, set_locale, COLS, LINES};
+use sl::{print_c51, print_d51, print_sl, update_locale, COLS, LINES};
 use std::fs;
 use std::io::{stdin, stdout, BufRead, BufReader, IsTerminal, Stdin, Write};
 use std::sync::{Arc, Mutex};
@@ -13,48 +13,18 @@ use std::sync::{Arc, Mutex};
 mod cli;
 mod sl;
 
-fn main() {
+fn main() -> Result<(), Error> {
     let args = CliOptions::parse();
     let stdin = stdin();
-    let names: Arc<Mutex<Vec<String>>> = if !Stdin::is_terminal(&stdin) {
-        let names = Arc::new(Mutex::new(vec![]));
-        let thread_names = Arc::clone(&names);
-        let stdin_file = FileDescriptor::dup(&stdin.lock()).unwrap();
-        std::thread::spawn(move || {
-            let reader = BufReader::new(stdin_file);
-            reader.lines().for_each(|line| match line {
-                Ok(line) => {
-                    let mut names = thread_names.lock().unwrap();
-                    names.push(line);
-                }
-                Err(_) => {}
-            });
-        });
-        names
-    } else if args.files {
-        Arc::new(Mutex::new(vec![]))
-    } else {
-        let mut files: Vec<String> = fs::read_dir(".")
-            .unwrap()
-            .filter_map(|p| match p {
-                Ok(p) => Some(String::from(p.file_name().to_str().unwrap())),
-                Err(_) => None,
-            })
-            .filter(|s| args.accident || !s.starts_with('.'))
-            .collect();
-        files.sort();
-        Arc::new(Mutex::new(files))
-    };
+    let names = get_car_names(&args, stdin)?;
+
+    terminal::enable_raw_mode()?;
 
     let mut stdout = stdout();
-    terminal::enable_raw_mode().unwrap();
-    stdout.execute(cursor::Hide).unwrap();
+    stdout.execute(cursor::Hide)?;
 
-    unsafe {
-        set_locale();
-    }
-
-    update_size().unwrap();
+    update_locale();
+    update_size()?;
 
     let print_train = if args.logo {
         print_sl
@@ -77,29 +47,29 @@ fn main() {
     }
 
     let mut x = unsafe { COLS - 1 };
-    stdout.queue(Clear(ClearType::All)).unwrap();
-    while print_train(
-        x,
-        &names
-            .lock()
-            .unwrap()
-            .iter()
-            .map(String::as_ref)
-            .collect::<Vec<&str>>(),
-    ) == 0
-    {
-        stdout.flush().unwrap();
+    stdout.queue(Clear(ClearType::All))?;
+    loop {
+        match names.lock() {
+            Ok(names) => {
+                if print_train(x, &names.iter().map(String::as_ref).collect::<Vec<&str>>()) != 0 {
+                    break;
+                }
+            }
+            Err(_) => break,
+        }
+
+        stdout.flush()?;
         x -= 1;
 
-        if crossterm::event::poll(time::Duration::from_micros(40_000)).unwrap() {
-            match read().unwrap() {
+        if crossterm::event::poll(time::Duration::from_micros(40_000))? {
+            match read()? {
                 Event::Key(KeyEvent {
                     code: KeyCode::Char('c'),
                     modifiers: KeyModifiers::CONTROL,
                     ..
                 }) => break,
                 Event::Resize(cols, lines) => unsafe {
-                    stdout.queue(Clear(ClearType::All)).unwrap();
+                    stdout.queue(Clear(ClearType::All))?;
                     COLS = cols as i32;
                     LINES = lines as i32;
                 },
@@ -108,13 +78,43 @@ fn main() {
         }
     }
 
-    stdout
-        .queue(Clear(ClearType::All))
-        .unwrap()
-        .queue(cursor::Show)
-        .unwrap();
-    stdout.flush().unwrap();
-    terminal::disable_raw_mode().unwrap();
+    stdout.queue(Clear(ClearType::All))?.queue(cursor::Show)?;
+    stdout.flush()?;
+    terminal::disable_raw_mode()?;
+
+    Ok(())
+}
+
+fn get_car_names(args: &CliOptions, stdin: Stdin) -> Result<Arc<Mutex<Vec<String>>>, Error> {
+    let names: Arc<Mutex<Vec<String>>> = if !Stdin::is_terminal(&stdin) {
+        let names = Arc::new(Mutex::new(vec![]));
+        let thread_names = Arc::clone(&names);
+        let stdin_file = FileDescriptor::dup(&stdin.lock())?;
+        std::thread::spawn(move || {
+            let reader = BufReader::new(stdin_file);
+            reader.lines().for_each(|line| match line {
+                Ok(line) => {
+                    let mut names = thread_names.lock().unwrap();
+                    names.push(line);
+                }
+                Err(_) => {}
+            });
+        });
+        names
+    } else if args.files {
+        Arc::new(Mutex::new(vec![]))
+    } else {
+        let mut files: Vec<String> = fs::read_dir(".")?
+            .filter_map(|p| match p {
+                Ok(p) => Some(String::from(p.file_name().to_str()?)),
+                Err(_) => None,
+            })
+            .filter(|s| args.accident || !s.starts_with('.'))
+            .collect();
+        files.sort();
+        Arc::new(Mutex::new(files))
+    };
+    Ok(names)
 }
 
 fn update_size() -> Result<(), Error> {
