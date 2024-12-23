8914a24f57b2092e877a3f020b6feea8447d5508
Mike Wallio <michael.wallio@outlook.com>
2024-12-23 02:42:20 +0000

    Remove unused

--- libsl/build.zig
+++ libsl/build.zig
@@ -5,9 +5,6 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const libsl = b.addStaticLibrary(.{ .name = "sl", .target = target, .optimize = optimize, .root_source_file = b.path("src/sl.zig") });
-
-    const zg = b.dependency("zg", .{});
-    libsl.root_module.addImport("grapheme", zg.module("grapheme"));
     libsl.addIncludePath(b.path("."));
     libsl.addCSourceFile(.{ .file = b.path("sl.c") });
 
--- libsl/src/sl.zig
+++ libsl/src/sl.zig
@@ -1,10 +1,6 @@
 const sl = @cImport(@cInclude("sl.h"));
 
 extern fn my_mvaddstr(y: i32, x: i32, str: [*:0]const u8) i32;
-//extern fn print_car(buffer: [*:0]u8, buffer_length: u32, format: [*:0]const u8, text: [*:0]const u8, text_display_width: u32) i32;
-
-extern var COLS: i32;
-extern var LINES: i32;
 
 export fn add_man(y: i32, x: i32) void {
     const man = [2][2][*:0]const u8{ [_][*:0]const u8{ "Help!", "" }, [_][*:0]const u8{ "\\O/", "(O)" } };
