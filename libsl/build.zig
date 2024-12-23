6bc170f3a78da99d1765e750d31a8afafa85e946
Mike Wallio <michael.wallio@outlook.com>
2024-12-23 02:54:18 +0000

    Separate into multiple libs

--- libsl/build.zig
+++ libsl/build.zig
@@ -4,9 +4,11 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const libsl = b.addStaticLibrary(.{ .name = "sl", .target = target, .optimize = optimize, .root_source_file = b.path("src/sl.zig") });
-    libsl.addIncludePath(b.path("."));
+    const slzig = b.addStaticLibrary(.{ .name = "slzig", .target = target, .optimize = optimize, .root_source_file = b.path("src/sl.zig") });
+    const libsl = b.addStaticLibrary(.{ .name = "slc", .target = target, .optimize = optimize });
+    slzig.addIncludePath(b.path("."));
     libsl.addCSourceFile(.{ .file = b.path("sl.c") });
 
     b.installArtifact(libsl);
+    b.installArtifact(slzig);
 }
