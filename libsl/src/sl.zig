c5c52dc22acfa9dc029b0141a7e6cfd324c134d7
Mike Wallio <michael.wallio@outlook.com>
2024-12-27 19:58:43 -0500

    Stop exporting smoke and man

--- libsl/src/sl.zig
+++ libsl/src/sl.zig
@@ -1,17 +1,6 @@
-const std = @import("std");
-const smoke = @import("add_smoke.zig");
-const man = @import("add_man.zig");
 const c51 = @import("c51.zig");
 const d51 = @import("d51.zig");
 const logo = @import("logo.zig");
-
-export fn add_smoke(y: i32, x: i32) void {
-    smoke.add_smoke(y, x);
-}
-
-export fn add_man(y: i32, x: i32) void {
-    man.add_man(y, x);
-}
 
 export fn add_C51(x: i32, namelist: [*][*:0]const u8, count: i32) i32 {
     return c51.add_C51(x, namelist[0..@as(usize, @intCast(count))]);
