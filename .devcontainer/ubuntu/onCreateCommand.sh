a50e424ccf2a55fff2850086a5eb09906b09eba7
Mike Wallio <michael.wallio@outlook.com>
2025-12-04 21:23:58 -0500

    Fix up devcontainer to work out of the box (#83)

--- .devcontainer/ubuntu/onCreateCommand.sh
+++ .devcontainer/ubuntu/onCreateCommand.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-sudo dnf install -y dnf-plugins-core cargo @c-development @rpm-development-tools rpkg gh
-
 # Setup Rust toolchain for wasm-pack
 rustup default stable
 rustup target add wasm32-unknown-unknown
