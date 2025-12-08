2b2bf88e18eb649f8749ea45acab14eda267f155
Mike Wallio <michael.wallio@outlook.com>
2025-12-08 01:15:51 +0000

    Remove router

--- apps/page/src/components/App.tsx
+++ apps/page/src/components/App.tsx
@@ -1,19 +1,5 @@
-import type { ComponentType } from 'preact';
-import Router from 'preact-router';
-import { lazy, Suspense } from 'preact/compat';
+import Home from './home/Home';
 
-type RouteProps = { path: string };
-
-const Home = lazy(() => import('./home/Home')) as ComponentType<RouteProps>;
-const EmbedPage = lazy(() => import('./embed/EmbedPage')) as ComponentType<RouteProps>;
-
-const App = () => (
-  <Suspense fallback={<div />}>
-    <Router>
-      <Home path="/" />
-      <EmbedPage path="/embed" />
-    </Router>
-  </Suspense>
-);
+const App = () => <Home />;
 
 export default App;
